c838923c9479ab2c0c61f8937622efeea3b981f1
Phoenix Nicholson <phoenix.nicholson2008@gmail.com>
2021-10-08 13:52:22 -0700

    Realized I messed up the testing, fixed all cypress tests

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 
 let num = (Math.floor(Math.random() * 50)) + 1;
 const genButton = document.getElementById('generate');
-const numSpan = document.getElementById('num');
+const numSpan = document.getElementById('random-num');
 
 genButton.addEventListener('click', ()=> {
     numSpan.textContent = num;
